a9580dcae98a35b71180f38b34eebcd2895e9881
ludovicMouly <ludovic.mouly@jaconsulting.fr>
2024-09-24 17:34:14 +0200

    fix tendance test

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 import { useState } from "react";
 import Navigation from "~/src/components/Sections/components/Navigation";
 import Classement from "~/src/components/Sections/widgets/Classement";
+import Tendances from "~/src/components/Sections/widgets/Tendances";
 
 const Home = () => {
   return (
     <div className="home">
       <Navigation />
       <Classement />
+      <Tendances />
     </div>
   );
 };
