6ea95ed276598497bc9321ece0bf76673ee53d0f
Ludovic Mouly <ludom82@gmail.com>
2024-09-30 15:59:52 +0200

    fix config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['cdn-transverse.azureedge.net'],
   },
   experimental: {
-    appDir: false,
+    appDir: true,
   },
 }
 
