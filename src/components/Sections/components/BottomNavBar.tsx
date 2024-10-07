930b2e80c3569054d5cf0fed306ab383cef1eff8
Ludovic Mouly <ludom82@gmail.com>
2024-10-07 03:17:00 +0200

    fix resp

--- src/components/Sections/components/BottomNavBar.tsx
+++ src/components/Sections/components/BottomNavBar.tsx
@@ -7,33 +7,33 @@ const BottomNavBar: React.FC = () => {
       className="fixed bottom-0 left-0 right-0 shadow-lg rounded-t-3xl"
       style={{
         backgroundColor: "rgba(9, 87, 159, 0.95)",
-        padding: "20px",
+        padding: "10px 0",
         zIndex: 1000,
       }}
     >
-      <div className="flex justify-between items-center p-2">
-         {/* Bouton Home */}
-         <Link href="/" className="flex flex-col items-center text-white">
-          <FaHome size={34} />
-          <span className="text-xs mt-1">Home</span>
+      <div className="flex justify-around items-center p-2">
+        {/* Bouton Home */}
+        <Link href="/" className="flex flex-col items-center text-white">
+          <FaHome className="text-[30px] sm:text-[34px]" />
+          <span className="text-[8px] sm:text-xs mt-1">Home</span>
         </Link>
 
         {/* Bouton Classement */}
         <Link href="/classement" className="flex flex-col items-center text-white">
-          <FaChartLine size={34} />
-          <span className="text-xs mt-1">Classement</span>
+          <FaChartLine className="text-[30px] sm:text-[34px]" />
+          <span className="text-[8px] sm:text-xs mt-1">Classement</span>
         </Link>
 
         {/* Bouton Matchs */}
         <Link href="/matchs" className="flex flex-col items-center text-white">
-          <FaCalendarAlt size={34} />
-          <span className="text-xs mt-1">Nos matchs</span>
+          <FaCalendarAlt className="text-[30px] sm:text-[34px]" />
+          <span className="text-[8px] sm:text-xs mt-1">Nos matchs</span>
         </Link>
 
         {/* Bouton Tous les Matchs */}
         <Link href="/tous-les-matchs" className="flex flex-col items-center text-white">
-          <FaList size={34} />
-          <span className="text-xs mt-1">Tous les Matchs</span>
+          <FaList className="text-[30px] sm:text-[34px]" />
+          <span className="text-[8px] sm:text-xs mt-1">Tous les Matchs</span>
         </Link>
       </div>
     </nav>
