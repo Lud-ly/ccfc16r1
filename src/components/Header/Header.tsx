a6203efdd14e986534ef0d689acf9cfc1c98376e
Ludovic Mouly <ludom82@gmail.com>
2024-10-07 14:37:49 +0200

    fine nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,15 +7,15 @@ import Navigation from "../Sections/components/Navigation";
 export const Header = () => {
   return (
     <div
-      className="p-4 flex flex-row justify-between items-center"
+      className="p-3 flex flex-row justify-between items-center"
       style={{ backgroundColor: "rgb(9, 87, 159)" }}
     >
       <Link href="/">
         <Image
           src="/images/logo.png"
           alt="logo ccfc"
-          width={60}
-          height={60}
+          width={50}
+          height={50}
           priority
         />
       </Link>
