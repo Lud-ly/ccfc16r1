6ddd991bb7ec5dda26fe7e522efb469ac04c2d95
Ludovic Mouly <ludom82@gmail.com>
2024-10-21 18:11:39 +0200

    📦 NEW:  remove stor hasShown

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,16 @@ import Link from "next/link";
 import Navigation from "../Sections/components/Navigation";
 
 export const Header = () => {
+  const handleLogoClick = () => {
+    localStorage.removeItem("hasShownSplash");
+  };
+
   return (
     <div
       className="p-1 flex flex-row justify-between items-center"
       style={{ backgroundColor: "rgb(9, 87, 159)" }}
     >
-      <Link href="/">
+      <Link href="/" onClick={handleLogoClick}>
         <Image
           src="/images/logo.png"
           alt="logo ccfc"
